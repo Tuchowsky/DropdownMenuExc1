be6b93c4db19275f8a1ad89ee78abb996c5675fc
Tuchowsky <arthur.tuchowsky@gmail.com>
2017-10-19 01:11:18 +0200

    Add Webfonts icons to appendiung HTML structure

--- js/main.js
+++ js/main.js
@@ -14,12 +14,15 @@ $(document).ready(function(){
                 <div class="sub-menu-triangle"></div>
                 <div class="sub-menu">
                     <div class="sub-menu-items">
+                        <i class="i_home"></i>
                         <a class="dropdown">Home</a>
                     </div>
                     <div class="sub-menu-items">
+                        <i class="i_mail"></i>
                         <a class="dropdown">Messages</a>
                     </div>
                     <div class="sub-menu-items">
+                        <i class="i_cog"></i>
                         <a class="dropdown">Settings</a>
                     </div>
                 </div>
