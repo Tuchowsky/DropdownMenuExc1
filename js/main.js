d47aff1fe4701b6a3a03ff7291a78ae6d5c706fc
Tuchowsky <arthur.tuchowsky@gmail.com>
2017-10-19 00:32:31 +0200

    Opening and closing Menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-    alert('insomnia');
+    let mainContainer = $('.main-container');
+    let navMenu = $('.nav-menu');
+
+    navMenu.click(function(){
+        let $this = $(this);
+        $this.toggleClass('open');
+        if($this.hasClass('open')){
+            mainContainer.append(`
+                <div class="sub-menu">
+                    <div class="sub-menu-items">
+                        <a class="dropdown">Home</a>
+                    </div>
+                    <div class="sub-menu-items">
+                        <a class="dropdown">Messages</a>
+                    </div>
+                    <div class="sub-menu-items">
+                        <a class="dropdown">Settings</a>
+                    </div>
+                </div>
+            `);
+        } else {
+            $this.removeClass('open');
+            $('div.sub-menu').remove();
+        }
+    });
 });
